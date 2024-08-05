3001fb242f9c0f93efc73c4b7799a0fee0bffe62
Moluguri Aravind <aravindakki123@gmail.com>
2024-08-05 19:13:44 +0530

    Add files via upload

---
+++ Inputmethods.js
@@ -0,0 +1,8 @@
+var x= prompt("Enter your name:");
+var z=confirm("weather your age>=18");
+if (z==true){
+    console.log("welcome "+ x + "!");
+}
+else{
+    console.log("Try when your age is 18 and 18 above");
+}
